2fbada32e24a65b40f45c6f771774252326d1faf
ADMIN <ADMIN@Radhagobinda.bbrouter>
2024-02-24 23:59:41 +0530

    n

--- src/test/java/Poshpeople/OrderFunctionality.java
+++ src/test/java/Poshpeople/OrderFunctionality.java
@@ -26,43 +26,96 @@ public class OrderFunctionality {
         Thread.sleep(10000);
         
         driver.findElement(By.xpath("//a[contains(text(),'				test			')]")).click();
-        Thread.sleep(10000);
+       
+        Thread.sleep(5000);
         //+ ve icon on product details page 
         driver.findElement(By.xpath("//input[contains(@value,'+')]")).click();
-        Thread.sleep(10000);
+        Thread.sleep(5000);
       //-ve icon on product details page
         driver.findElement(By.xpath("//input[contains(@value,'-')]")).click();
-        Thread.sleep(10000);
+        Thread.sleep(5000);
        WebElement numberfield = driver.findElement(By.xpath("//input[contains(@class,'input-text qty text')]"));
        numberfield.clear();
-       Thread.sleep(10000);
+       Thread.sleep(5000);
        numberfield.sendKeys("4");
-       Thread.sleep(10000);
+       Thread.sleep(5000);
        //Add to cart button 
        driver.findElement(By.xpath("//button[contains(text(),'Add to cart')]")).click();
-       Thread.sleep(10000);
+       Thread.sleep(5000);
        driver.findElement(By.xpath("(//i[contains(@class,'eicon-bag-medium')])[1]")).click();
-       Thread.sleep(10000);
+       Thread.sleep(5000);
        driver.findElement(By.xpath("(//div[contains(@class,'elementor-menu-cart__close-button')])[1]")).click();
-       Thread.sleep(10000);
+       Thread.sleep(5000);
+       WebElement qty = driver.findElement(By.xpath("(//span[contains(text(),'4')])[1]"));
+       Thread.sleep(5000);
+       String actualqty = "4";
+       Thread.sleep(5000);
+
+       String expectedqty = qty.getText();
+       Thread.sleep(5000);
+
+       System.out.println(expectedqty);
+       Thread.sleep(5000);
+
+      if(actualqty.equals(expectedqty)) {
+    	  System.out.println("Add product quantity is matching with the cart quantity");
+      }
+      else {
+    	  System.out.println("Add product quantity is matching with the cart quantity");
+      }
+       Thread.sleep(5000);
        driver.findElement(By.xpath("(//button[contains(text(),'Buy Now')])[1]")).click();
+       Thread.sleep(5000);
        String actualurl = driver.getCurrentUrl();
        String expecteurl = "https://poshpeople.in/checkout/";
-       Thread.sleep(10000);
+       Thread.sleep(5000);
        System.out.println(actualurl);
+      
        if(actualurl.equals(expecteurl)) {
-    	   System.out.println("actualurl is  matching with the expected url,program succesfully executed ");
+    	   System.out.println("actualurl is  matching with the expected url,program succesfully executed. ");
        }
        else {
     	   System.out.println("actualurl is not matching with the expected url");
        }
+        
        
+       driver.navigate().back();
+       Thread.sleep(10000);
+       // click on cart button 
+       driver.findElement(By.xpath("(//i[contains(@class,'eicon-bag-medium')])[1]")).click();
+       Thread.sleep(5000);
+       // click on view cart button 
+       driver.findElement(By.xpath("(//span[contains(text(),'View cart')])[1]")).click();
+       Thread.sleep(5000);
+       driver.findElement(By.xpath("(//input[contains(@class,'plus')])[3]")).click();
+       Thread.sleep(5000);
+       driver.findElement(By.xpath("(//input[contains(@class,'minus')])[3]")).click();
+       Thread.sleep(5000);
+     WebElement t =   driver.findElement(By.xpath(" (//input[contains(@class,'input-text qty text')])[3]"));
+     Thread.sleep(5000);
+      t.clear();
+      Thread.sleep(5000);
+      t.sendKeys("4");
+      // click on update cart
+      driver.findElement(By.xpath("(//button[contains(text(),'Update cart')])")).click();
+      Thread.sleep(10000);
+      driver.findElement(By.xpath("(//a[normalize-space(text())='Proceed to checkout'])")).click();
+      Thread.sleep(5000);
+   
       
-        Thread.sleep(10000);
-        
-        
-        
-        
+      String actualurl1 = driver.getCurrentUrl();
+      Thread.sleep(5000);
+      String expecteur1 = "https://poshpeople.in/checkout/";
+      Thread.sleep(5000);
+      System.out.println(actualurl);
+     
+      if(actualurl.equals(expecteurl)) {
+   	   System.out.println("actualurl is  matching with the expected url,user naviagates through the cart button and program is succesfully executed. ");
+      }
+      else {
+   	   System.out.println("actualurl is not matching with the expected url");
+      }
+       Thread.sleep(10000);
         
         driver.quit();
         
@@ -70,7 +123,6 @@ public class OrderFunctionality {
         
 		
 	}
-	
 }
 
 
